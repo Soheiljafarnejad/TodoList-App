21d49d198af71c7f916a78e4df5021b8b2a29577
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-03-04 22:29:10 +0330

    add navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import TodoApp from "./Component/TodoApp";
 import "./App.css";
 import HeaderCom from "./Component/Header/HeaderCom";
 import TodoContext from "./Component/Context/TodoContext";
+import NavBar from "./Component/NavBar/Navbar";
+import { useState } from "react";
 const App = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <>
       <TodoContext>
-      <HeaderCom />
+        {toggle && <NavBar setToggle={setToggle} />}
+        <HeaderCom setToggle={setToggle} />
         <TodoApp />
       </TodoContext>
     </>
--- src/Component/Header/HeaderCom.js
+++ src/Component/Header/HeaderCom.js
@@ -1,37 +1,41 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { BiSearchAlt } from "react-icons/bi";
 import { BiMenu } from "react-icons/bi";
 import { useTodosAction } from "../Context/TodoContext.js";
 import style from "./HeaderCom.module.css";
 
-const HeaderCom = () => {
+const HeaderCom = ({ setToggle }) => {
   const { searchHandler } = useTodosAction();
   const [isSearch, setIsSearch] = useState(false);
 
-  const onSearch = (e) => {
+  const searchValue = (e) => {
     searchHandler(e.target.value);
   };
 
-  const clickHandler = () => {
+  const onSearch = () => {
     setIsSearch(!isSearch);
   };
 
+  const onClickMenu = () => {
+    setToggle(true);
+  };
+
   return (
-    <header className="container">
+    <header className={`container ${style.container}`}>
       <section className={style.header}>
         <div className={style.icons}>
-          <BiMenu />
+          <BiMenu onClick={onClickMenu} className={style.menu} />
           <div className={style.searchBar}>
             {isSearch && (
               <input
-                onChange={onSearch}
+                onChange={searchValue}
                 className={style.search}
                 type="text"
                 autoFocus={true}
                 placeholder="search..."
               />
             )}
-            <BiSearchAlt onClick={clickHandler} className={style.iconSearch} />
+            <BiSearchAlt onClick={onSearch} className={style.iconSearch} />
           </div>
         </div>
         <h1 className={style.title}>What's up joy!</h1>
